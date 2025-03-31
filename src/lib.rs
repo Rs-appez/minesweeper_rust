3f031bf76da5dfe2c6febc6f4be695f390d91576
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-03-31 03:23:05 +0200

    add mines generation

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,33 @@
+mod random;
+
+use random::random_number;
+
 type Position = (usize, usize);
 
 struct MineSweeper {
     width: usize,
     height: usize,
-    open_tiles: Vec<Position>,
+    open_cells: Vec<Position>,
     mines: Vec<Position>,
-    flagged_tiles: Vec<Position>,
+    flagged_cells: Vec<Position>,
 }
+
+impl MineSweeper {
+    pub fn new(width: usize, height: usize, mine_count: usize) -> MineSweeper {
+        MineSweeper {
+            width,
+            height,
+            open_cells: Vec::new(),
+            mines: {
+                let mut mines: Vec<Position> = Vec::new();
+
+                while mines.len() < mine_count{
+                    mines.push((random_number(width), random_number(height)));
+                }
+                mines
+            },
+            flagged_cells: Vec::new(),
+        }
+    }
+}
+
